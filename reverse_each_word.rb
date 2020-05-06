ece2223119b90a73e04596d98841617ea65017ee
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-06 18:44:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,3 @@
-# def reverse_each_word(string)
-#   split_string = string.split
-#   split_string.collect {|word| word.reverse}.join(' ')
-# end
 
 def reverse_each_word(string)
   string.split.collect {|word| word.reverse}.join(' ')
